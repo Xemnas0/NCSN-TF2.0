cd068d87fd58f9e642438c9bf33130337900b966
fnuzzo <fnuzzo@kth.se>
2019-10-10 14:50:52 +0200

    add MultiResolutionFusion block

--- model/layers.py
+++ model/layers.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import keras.layers as layers
 
+
 class ConditionalFullPreActivationBlock(layers.Layer):
-    def __init__(self, activation, downsample, filters, kernel_size, dilation, preactivation_blocks):
+    def __init__(self, activation, downsample, filters, kernel_size, dilation):
         super(ConditionalFullPreActivationBlock, self).__init__()
         # todo: check why 2 preactivation_blocks blocks, does it work with 1?
 
@@ -31,6 +32,7 @@ class ConditionalInstanceNormalizationPlusPlus2D(layers.Layer):
     def __init__(self, C, L):
         pass
 
+
 # class ResidualConvUnit(layers.Layer):
 #     def __init__(self, filters, kernel_size=3):
 #         super(ResidualConvUnit, self).__init__()
@@ -47,7 +49,28 @@ class ConditionalInstanceNormalizationPlusPlus2D(layers.Layer):
 #         return tf.add(x, input)
 #
 class MultiResolutionFusion(layers.Layer):
-    def __init__(self, filters, kernel_size=3):
+    def __init__(self, filters, kernel_size=3, has_low_input=True):
         super(MultiResolutionFusion, self).__init__()
 
+        self.has_low_input = has_low_input
 
+        if has_low_input:
+            self.conv2d_low = layers.Conv2D(filters, kernel_size, padding='same')
+        self.conv2d_high = layers.Conv2D(filters, kernel_size, padding='same')
+
+    def call(self, inputs, **kwargs):
+        if len(inputs) == 1:
+            high_input = inputs
+            x = self.conv2d_high(high_input)
+            return x
+        elif len(inputs) == 2:
+            high_input, low_input = inputs
+
+            # FIXME: make me any beautiful
+            upsample = layers.UpSampling2D(high_input.shape)
+
+            low_input = self.conv2d_low(low_input)
+            low_input = upsample(low_input)
+            high_input = self.conv2d_high(high_input)
+
+            return low_input + high_input
