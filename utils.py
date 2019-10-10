7c150d58862cd39cfb360f1d185a565c646856c0
fnuzzo <fnuzzo@kth.se>
2019-10-10 14:32:32 +0200

    set mnist as default dataset

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import argparse
 
 def get_command_line_args():
     parser = argparse.ArgumentParser(description='I AM A HELP MESSAGE')
-    parser.add_argument('--dataset', default='cifar10',
-                        help="tfds name of dataset. Default: 'cifar10'")
+    parser.add_argument('--dataset', default='mnist',
+                        help="tfds name of dataset. Default: 'mnist'")
     # TODO: add more command line arguments here!
     # possible: whether to use local data, where data is stored, checkpointing
 
