15b9654ef466ddc2b75356ae86075af6e724eb21
Eliisabet Hein <s1447681@sms.ed.ac.uk>
2019-10-13 19:27:05 +0200

    add progress bar printing

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import configs
 from datasets.dataset_loader import get_data_generator
 from model.refinenet import RefineNet
 from losses.losses import loss_per_batch
-
+from tqdm import tqdm
 
 def train(model, inputs, learning_rate):
     pass
@@ -17,12 +17,13 @@ if __name__ == "__main__":
 
     # loading dataset
     train_data = get_data_generator(args.dataset)
+    num_batches = tf.data.experimental.cardinality(train_data)
 
     # initialize model
     model = RefineNet(filters=5, activation=tf.nn.elu)
 
     # declare optimizer
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01) # 10 times larger than in their paper
 
     # TRAINING LOOP CAN GO HERE
     epochs = 3
@@ -30,20 +31,23 @@ if __name__ == "__main__":
     # array of sigma levles
     sigma_levels = tf.math.exp(tf.linspace(tf.math.log(args.sigma_low), tf.math.log(args.sigma_high), args.num_L))
 
+    print("=========================================")
     for epoch in range(epochs):
-        print("epoch", epoch)
-
-        for i, data_batch in enumerate(train_data):
+        progress_bar = tqdm(enumerate(train_data))
+        progress_bar.set_description(f'loss {0:.3f}')
+        for i, data_batch in progress_bar:
             idx_sigmas = tf.random.uniform([data_batch.shape[0]], minval=0, maxval=args.num_L, dtype=tf.dtypes.int32)
             sigmas = tf.gather(sigma_levels, idx_sigmas)
             sigmas = tf.reshape(sigmas, shape=(data_batch.shape[0], 1, 1, 1))
             data_batch_perturbed = data_batch + tf.random.normal(shape=data_batch.shape) * sigmas
 
             with tf.GradientTape() as t:
-                t.watch(data_batch_perturbed)
                 scores = model([data_batch_perturbed, idx_sigmas])
                 batch_loss = loss_per_batch(scores, data_batch_perturbed, data_batch, sigmas)
                 gradients = t.gradient(batch_loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-            print("epoch:", epoch, "loss:", batch_loss)
+            progress_bar.set_description(f'loss {batch_loss:.3f}')
+    print("=========================================")
+
+    # NOTE bad way to choose the best model - saving all checkpoints and then testing after
