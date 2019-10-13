a2d406c6a6a6a13f50d731acd2ec6a7375284384
Matosevic <antonio.matosevic96@gmail.com>
2019-10-13 17:53:44 +0200

    implemented the loss

--- losses/losses.py
+++ losses/losses.py
@@ -1,3 +1,14 @@
-def score_matching(x_approx, x, sigmas):
+import tensorflow as tf
+import configs
 
-    pass
+#@tf.function
+def loss_per_batch(score, x_perturbed, x, sigmas):
+    l_batch = tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=(1, 2)) #norm over height, width
+    l_batch = tf.norm(l_batch, axis=-1) # norm over channels (1 or 3)
+    l_batch = tf.square(l_batch)
+
+    #l_batch = tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
+    #l_batch += tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
+    #l_batch += tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
+
+    return 0.5 * tf.reduce_mean(l_batch) / configs.config_values.num_L
--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ def get_command_line_args():
                         help="tfds name of dataset. Default: 'mnist'")
     parser.add_argument('--num_L', default=10,
                         help="How many levels of noise to use. Default: 10")
+    parser.add_argument('--sigma_low', default=0.01,
+                        help="Lowest sigma? Default 0.01")
+    parser.add_argument('--sigma_high', default=1.0,
+                        help="Highest sigma? Default 1")
     # TODO: add more command line arguments here!
     # possible: whether to use local data, where data is stored, checkpointing
 
