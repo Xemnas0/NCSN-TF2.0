c4411ab395460b84ca59e08f5eb61434c4cca17d
fnuzzo <fnuzzo@kth.se>
2019-10-13 15:05:36 +0200

    forward pass working

--- model/layers.py
+++ model/layers.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import keras.layers as layers # TODO: check if we should use keras or tf.keras
+import tensorflow.keras.layers as layers  # TODO: check if we should use keras or tf.keras
 import configs
 
 
@@ -11,14 +11,11 @@ class DilatedConv2D(layers.Layer):
         self.conv = layers.Conv2D(filters, kernel_size, strides=strides, dilation_rate=dilation, padding='valid')
         self.filters = filters
 
-    def build(self, input_shape):
-        print(f"Output shape: {self.compute_output_shape(input_shape)}")
-
-    def compute_output_shape(self, input_shape):
-        # print(f"input shape: {input_shape}")
-
-        output_shape = (input_shape[0], input_shape[1], input_shape[2], self.filters)
-        return output_shape
+    # def compute_output_shape(self, input_shape):
+    #     # print(f"input shape: {input_shape}")
+    #
+    #     output_shape = (input_shape[0], input_shape[1], input_shape[2], self.filters)
+    #     return output_shape
 
     def call(self, inputs, **kwargs):
         x = self.padding(inputs)
@@ -40,9 +37,16 @@ class ConditionalFullPreActivationBlock(layers.Layer):
         self.activation = activation
 
         self.increase_channels_skip = layers.Conv2D(filters, kernel_size=1, padding='same')
-        
-    def build(self, input_shape):
-        print(f"Output shape: {self.compute_output_shape(input_shape)}")
+
+        self.filters = filters
+
+    # def build(self, input_shape):
+    #     print(f"Output shape: {self.compute_output_shape(input_shape)}")
+
+    # def compute_output_shape(self, input_shape):
+    #     print("input shape residual block: "+str(input_shape))
+    #     output_shape = (input_shape[0][0], input_shape[0][1], input_shape[0][2], self.filters)
+    #     return output_shape
 
     def call(self, inputs, **kwargs):
         skip_x, idx_sigmas = inputs
@@ -73,12 +77,13 @@ class ConditionalInstanceNormalizationPlusPlus2D(layers.Layer):
         super(ConditionalInstanceNormalizationPlusPlus2D, self).__init__()
         self.L = configs.config_values.num_L
 
-        self.init_weights = 'random_normal' #tf.initializers.RandomNormal(1, 0.02)
+        self.init_weights = 'random_normal'  # tf.initializers.RandomNormal(1, 0.02)
         self.init_bias = 'zeros'
 
     def build(self, input_shape):
         self.C = input_shape[0][-1]  # FIXME: I might not be what you think I am. Zero?
-        self.alpha = self.add_weight(shape=(self.L, 1, 1, self.C), initializer=self.init_weights) # TODO: maybe change init
+        self.alpha = self.add_weight(shape=(self.L, 1, 1, self.C),
+                                     initializer=self.init_weights)  # TODO: maybe change init
         self.beta = self.add_weight(shape=(self.L, 1, 1, self.C), initializer=self.init_bias)
         self.gamma = self.add_weight(shape=(self.L, 1, 1, self.C), initializer=self.init_weights)
 
@@ -104,10 +109,11 @@ class ConditionalChainedResidualPooling2D(layers.Layer):
         super(ConditionalChainedResidualPooling2D, self).__init__()
         self.activation1 = activation
         self.n_blocks = n_blocks
+        self.pooling_size = pooling_size
         for n in range(n_blocks):
             setattr(self, f'norm{n}', ConditionalInstanceNormalizationPlusPlus2D())
             setattr(self, f'conv{n}', layers.Conv2D(filters, kernel_size, padding='same'))
-        self.pooling = layers.AveragePooling2D(pooling_size, padding='same')
+        # self.pooling = layers.AveragePooling2D(pooling_size, padding='same')
 
     def call(self, inputs, **kwargs):
         x, idx_sigmas = inputs
@@ -117,7 +123,8 @@ class ConditionalChainedResidualPooling2D(layers.Layer):
             norm = getattr(self, f'norm{n}')
             conv = getattr(self, f'conv{n}')
             x = norm([x, idx_sigmas])
-            x = self.pooling(x)
+            # x = self.pooling(x)
+            x = tf.nn.avg_pool2d(x, self.pooling_size, strides=1, padding='SAME')
             x = conv(x)
             x_residual += x
         return x_residual
@@ -163,14 +170,15 @@ class MultiResolutionFusion(layers.Layer):
             x = self.conv2d_high(x)
             return x
         elif len(inputs[0]) == 2:
-            high_input, low_input = inputs
+            high_input, low_input = inputs[0]
 
             # FIXME: make me any beautiful
-            upsample = layers.UpSampling2D(high_input.shape)
+            # upsample = layers.UpSampling2D(high_input.shape[1:-1])
 
             low_input = self.norm_low([low_input, idx_sigmas])
             low_input = self.conv2d_low(low_input)
-            low_input = upsample(low_input)
+            # low_input = upsample(low_input)
+            low_input = tf.image.resize(low_input, high_input.shape[1:-1])
             high_input = self.norm_high([high_input, idx_sigmas])
             high_input = self.conv2d_high(high_input)
 
@@ -201,23 +209,22 @@ class RefineBlock(layers.Layer):
             self.rcu_low = RCUBlock(self.activation, self.filters, self.kernel_size)
 
     def call(self, inputs, **kwargs):
-        low_input = None
         idx_sigmas = inputs[1]
         if len(inputs[0]) == 1:
             high_input = inputs[0][0]
 
-            high_input = self.rcu_high(high_input)
-            x = self.mrf([high_input])
+            high_input = self.rcu_high([high_input, idx_sigmas])
+            x = self.mrf([[high_input], idx_sigmas])
 
         elif len(inputs[0]) == 2:
             high_input, low_input = inputs[0]
 
-            high_input = self.rcu_high(high_input)
-            low_input = self.rcu_low(low_input)
-            x = self.mrf([high_input, low_input])
+            high_input = self.rcu_high([high_input, idx_sigmas])
+            low_input = self.rcu_low([low_input, idx_sigmas])
+            x = self.mrf([[high_input, low_input], idx_sigmas])
 
-        x = self.crp(x, idx_sigmas)
-        x = self.rcu_end(x, idx_sigmas)
+        x = self.crp([x, idx_sigmas])
+        x = self.rcu_end([x, idx_sigmas])
         return x
 
 # class TestLayer(layers.Layer):
--- model/refinenet.py
+++ model/refinenet.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
-import keras
-import keras.layers as layers
+import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
 
-from model.layers import RefineBlock, ConditionalFullPreActivationBlock, DilatedConv2D, ConditionalInstanceNormalizationPlusPlus2D
+from model.layers import RefineBlock, ConditionalFullPreActivationBlock, DilatedConv2D, \
+    ConditionalInstanceNormalizationPlusPlus2D
+
 
 class RefineNet(keras.Model):
 
@@ -11,16 +13,17 @@ class RefineNet(keras.Model):
 
         self.increase_channels = layers.Conv2D(filters, kernel_size=3, padding='same')
 
-        self.preact_1 = ConditionalFullPreActivationBlock(activation, filters, kernel_size=3, pool_size=7) #FIXME: check pool size
-        self.preact_2 = ConditionalFullPreActivationBlock(activation, filters*2, kernel_size=3)
-        self.preact_3 = ConditionalFullPreActivationBlock(activation, filters*2, kernel_size=3, dilation=2, padding=2)
-        self.preact_4 = ConditionalFullPreActivationBlock(activation, filters*2, kernel_size=3, dilation=4, padding=4)
+        self.preact_1 = ConditionalFullPreActivationBlock(activation, filters, kernel_size=3,
+                                                          pool_size=4)  # FIXME: check pool size
+        self.preact_2 = ConditionalFullPreActivationBlock(activation, filters * 2, kernel_size=3)
+        self.preact_3 = ConditionalFullPreActivationBlock(activation, filters * 2, kernel_size=3, dilation=2, padding=2)
+        self.preact_4 = ConditionalFullPreActivationBlock(activation, filters * 2, kernel_size=3, dilation=4, padding=4)
         # Increasing the dilation even more would not help
 
         self.refine_block_1 = RefineBlock(2, activation, filters)
-        self.refine_block_2 = RefineBlock(2, activation, filters*2)
-        self.refine_block_3 = RefineBlock(2, activation, filters*2)
-        self.refine_block_4 = RefineBlock(2, activation, filters*2)
+        self.refine_block_2 = RefineBlock(2, activation, filters * 2)
+        self.refine_block_3 = RefineBlock(2, activation, filters * 2)
+        self.refine_block_4 = RefineBlock(2, activation, filters * 2)
 
         self.norm = ConditionalInstanceNormalizationPlusPlus2D()
         self.activation = activation
@@ -28,12 +31,12 @@ class RefineNet(keras.Model):
 
     def build(self, input_shape):
         # Here we get the depth of the image that is passed to the model at the start, i.e. 1 for MNIST.
-        self.decrease_channels = layers.Conv2D(input_shape[1], kernel_size=3, stride=1, padding=1)
+        self.decrease_channels = layers.Conv2D(input_shape[0][-1], kernel_size=3, strides=1, padding='same')
 
     def call(self, inputs, mask=None):
         x, idx_sigmas = inputs
-
-        x = self.increase_channels(inputs)
+        print(x.shape)
+        x = self.increase_channels(x)
 
         output_1 = self.preact_1([x, idx_sigmas])
         output_2 = self.preact_2([output_1, idx_sigmas])
@@ -52,4 +55,13 @@ class RefineNet(keras.Model):
         return output
 
 
+if __name__ == '__main__':
+    import tensorflow_datasets as tfds
 
+    data_generators = tfds.load(name="cifar10", split="test", batch_size=-1)
+    test = tf.cast(data_generators['image'], tf.float32)
+
+    x = tf.expand_dims(test[:2], 0)
+    idx_sigmas = tf.convert_to_tensor([3, 9])
+    model = RefineNet(5, tf.nn.elu)
+    output = model([x, idx_sigmas])
