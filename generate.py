7084f7dab104c28d37f709f6666636229062f4f4
fnuzzo <fnuzzo@kth.se>
2019-10-15 22:52:49 +0200

    save samples to file

--- generate.py
+++ generate.py
@@ -4,6 +4,8 @@ import utils
 import configs
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from datetime import datetime
+import os
 
 def clamped(x):
     return tf.clip_by_value(x, 0, 1.0)
@@ -12,6 +14,11 @@ def clamped(x):
 def plot_grayscale(image):
     plt.imshow(image, cmap=plt.get_cmap("gray"))
     plt.show()
+
+
+def save_image(image, dir):
+    plt.imshow(image, cmap=plt.get_cmap("gray"))
+    plt.savefig(dir)
 
 
 def sample(model, sigmas, eps=2 * 1e-5, T=100, n_images=1):
@@ -26,7 +33,7 @@ def sample(model, sigmas, eps=2 * 1e-5, T=100, n_images=1):
     image_size = (n_images, 28, 28, 1)
 
     x = tf.random.uniform(shape=image_size)
-    plot_grayscale(x[0, :, :, 0])
+    # plot_grayscale(x[0, :, :, 0])
 
     for i, sigma_i in enumerate(sigmas):
         print(f"sigma {i}/{len(sigmas)}")
@@ -35,10 +42,10 @@ def sample(model, sigmas, eps=2 * 1e-5, T=100, n_images=1):
         for t in tqdm(range(T)):
             z_t = tf.random.normal(shape=image_size, mean=0, stddev=1.0)  # TODO: check if stddev is correct
             score = model([x, tf.ones(n_images, dtype=tf.int32) * i])
-            x = x + alpha_i * score + tf.sqrt(alpha_i*2) * z_t
+            x = x + alpha_i * score + tf.sqrt(alpha_i * 2) * z_t
 
-            plot_grayscale(clamped(x[0, :, :, 0]))
-            
+        # plot_grayscale(clamped(x[0, :, :, 0]))
+
     return x
 
 
@@ -46,9 +53,12 @@ if __name__ == '__main__':
     args = utils.get_command_line_args()
     configs.config_values = args
 
-    model_name = ''
+    start_time = datetime.now().strftime("%y%m%d-%H%M%S")
+
     model_directory = './saved_models/'
     dataset = 'mnist/'
+    samples_directory = './samples/' + start_time + "/"
+    os.makedirs(samples_directory)
 
     step = tf.Variable(0)
     model = RefineNet(filters=16, activation=tf.nn.elu)
@@ -63,5 +73,8 @@ if __name__ == '__main__':
                                            tf.math.log(0.01),
                                            10))
 
-    samples = sample(model, sigma_levels, T=100)
+    samples = sample(model, sigma_levels, T=100, n_images=50)
 
+    for i, sample in enumerate(samples):
+        # plot_grayscale(sample[:, :, 0])
+        save_image(sample[:, :, 0], samples_directory + f'sample_{i}.png')
