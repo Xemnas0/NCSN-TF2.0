76d2c09bb21b320dc6fbb30593bf773199f84b3c
Matosevic <antonio.matosevic96@gmail.com>
2019-10-22 21:15:03 +0200

    ssm loss ok, but memory error

--- losses/losses.py
+++ losses/losses.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 import configs
 
-@tf.function
-def loss_per_batch(score, x_perturbed, x, sigmas):
-    l_batch = tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=(1, 2)) #norm over height, width
-    l_batch = tf.norm(l_batch, axis=-1) # norm over channels (1 or 3)
-    l_batch = tf.square(l_batch)
-
-    #l_batch = tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
-    #l_batch += tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
-    #l_batch += tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
-
-    return 0.5 * tf.reduce_mean(l_batch) / configs.config_values.num_L
+# @tf.function
+# def loss_per_batch(score, x_perturbed, x, sigmas):
+#     l_batch = tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=(1, 2)) #norm over height, width
+#     l_batch = tf.norm(l_batch, axis=-1) # norm over channels (1 or 3)
+#     l_batch = tf.square(l_batch)
+#
+#     #l_batch = tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
+#     #l_batch += tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
+#     #l_batch += tf.norm(sigmas * score + (x_perturbed - x) / sigmas, axis=-1)
+#
+#     return 0.5 * tf.reduce_mean(l_batch) / configs.config_values.num_L
 
 @tf.function
 def loss_per_batch_alternative(score, x_perturbed, x, sigmas):
@@ -21,11 +21,21 @@ def loss_per_batch_alternative(score, x_perturbed, x, sigmas):
     return loss
 
 @tf.function
-def ssm_loss(scores, data_grads):
-    # TODO: CHECK IF SCORE AND DATA_GRADS OF SAME DIM.?
-    v = tf.random.normal(scores.shape)
-    # FIXME: should v be normalized?
-    loss = tf.transpose(v) * data_grads * v + 0.5 * tf.square(tf.transpose(v) * scores)
-    loss = tf.reduce_mean(loss)
-    tf.print(loss)
+def ssm_loss(score_net, data_batch):
+    sum_over = list(range(1, len(data_batch.shape)))
+    v = tf.random.normal(data_batch.shape)
+    with tf.GradientTape() as t:
+        t.watch(data_batch)
+        scores = score_net(data_batch)
+        v_times_scores = tf.reduce_sum(v * scores)
+    grad = t.gradient(v_times_scores, data_batch)
+    loss_first = tf.reduce_sum(grad * v, axis=sum_over)
+    loss_second = 0.5 * tf.reduce_sum(tf.square(scores), axis=sum_over)
+    loss = tf.reduce_mean(loss_first + loss_second)
+
+    return loss
+
+    # loss = tf.transpose(v) * data_grads * v + 0.5 * tf.square(tf.transpose(v) * scores)
+    # loss = tf.reduce_mean(loss)
+    # tf.print(loss)
     return loss
---
+++ model/modelresnet.py
@@ -0,0 +1,118 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.layers as layers
+
+
+class ModelResNet(keras.Model):
+
+    def __init__(self, activation):
+        super(ModelResNet, self).__init__()
+
+        self.increase_channels = layers.Conv2D(32, kernel_size=3, padding='same')
+
+        self.res_enc1 = ResidualBlock(activation, 32, is_encoder=True)
+        self.res_enc2 = ResidualBlock(activation, 64, is_encoder=True, resize=True)
+        self.res_enc3 = ResidualBlock(activation, 64, is_encoder=True)
+        self.res_enc4 = ResidualBlock(activation, 128, is_encoder=True, resize=True)
+        self.res_enc5 = ResidualBlock(activation, 128, is_encoder=True)
+
+        self.res_dec1 = ResidualBlock(activation, 128, is_encoder=False)
+        self.res_dec2 = ResidualBlock(activation, 128, is_encoder=False, resize=True)
+        self.res_dec3 = ResidualBlock(activation, 64, is_encoder=False)
+        self.res_dec4 = ResidualBlock(activation, 64, is_encoder=False, resize=True)
+        self.res_dec5 = ResidualBlock(activation, 32, is_encoder=False)
+
+        self.decrease_channels = None
+
+    def build(self, input_shape):
+        # Here we get the depth of the image that is passed to the model at the start, i.e. 1 for MNIST.
+        self.in_shape = input_shape
+        self.decrease_channels = layers.Conv2D(input_shape[-1], kernel_size=3, strides=1, padding='same')
+
+    def call(self, inputs, mask=None):
+        inputs = inputs
+        out = self.increase_channels(inputs)
+
+        out = self.res_enc1(out)
+        out = self.res_enc2(out)
+        out = self.res_enc3(out)
+        out = self.res_enc4(out)
+        out = self.res_enc5(out)
+
+        out = self.res_dec1(out)
+        out = self.res_dec2(out)
+        out = self.res_dec3(out)
+        out = self.res_dec4(out)
+        out = self.res_dec5(out)
+
+        out = self.decrease_channels(out)
+
+        return out
+
+
+class ResidualBlock(layers.Layer):
+    def __init__(self, activation, filters, is_encoder, kernel_size=3, resize=False):
+        super(ResidualBlock, self).__init__()
+
+        # FIXME: THEY USE GROUP NORMALIZATION, NOT SURE IF THIS MAKES A DIFFERENCE?
+        self.norm1 = layers.BatchNormalization()
+        self.norm2 = layers.BatchNormalization()
+        self.activation = activation
+
+        self.filters = filters
+        self.is_encoder = is_encoder
+        self.resize = resize
+        self.strides = strides = 2 if resize else 1
+        padding = 'same' #if strides == 1 else 'valid'
+
+        if is_encoder:
+            self.conv1 = layers.Conv2D(filters, kernel_size, strides=strides, padding=padding)
+            self.conv2 = layers.Conv2D(filters, kernel_size, strides=1, padding='same')
+        else:
+            self.conv1 = layers.Conv2DTranspose(filters, kernel_size, strides=1, padding='same')
+            self.conv2 = layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding=padding)
+
+        self.increase_channels_skip = layers.Conv2D(filters, kernel_size=1, strides=strides, padding=padding)
+
+    def build(self, input_shape):
+        self.increase_skip_size = layers.Conv2D(self.filters, kernel_size=1, strides=2)
+        self.decrease_skip_size = layers.Conv2DTranspose(self.filters, kernel_size=1, strides=self.strides)
+
+    def call(self, inputs, **kwargs):
+        # TODO: CHECK IF INPUTS NEED TO BE SPLIT ?
+        print(self.name)
+        x = self.norm1(inputs)
+        x = self.activation(x)
+        x = self.conv1(x)
+        x = self.norm2(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+
+        # FIXME: THEY DO IT WITH STRIDE = 2. ALSO, THEY RESIZE x IN THE FIRS CONVOLUTION.
+        # if self.resize:
+        #     if self.is_encoder:
+        #         a = x.shape[1:-1].as_list()[0] * 2
+                # b = x.shape[1:-1].as_list()[1] * 2
+                # new_dims = tf.TensorShape([a, b])
+                # skip_x = self.increase_channels_skip(inputs)
+            # else:
+                # a = x.shape[1:-1].as_list()[0] / 2
+                # b = x.shape[1:-1].as_list()[1] / 2
+                # new_dims = tf.TensorShape([a, b])
+                # skip_x = self.decrease_channels_skip(inputs)
+            # x = tf.image.resize(x, new_dims)
+            # skip_x = tf.image.resize(inputs, new_dims)  # TODO: CHECK IF OK?
+
+        # else:
+        #     skip_x = inputs
+
+        if x.shape != inputs.shape:
+            if self.is_encoder:
+                skip_x = self.increase_channels_skip(inputs)
+            else:
+                skip_x = self.decrease_skip_size(inputs)
+        else:
+            skip_x = inputs
+        print(skip_x.shape)
+        print(x.shape)
+        return skip_x + x
