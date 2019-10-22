7568154b52ba97338eedc515e087edd79932ec3c
Matosevic <antonio.matosevic96@gmail.com>
2019-10-22 15:40:06 +0200

    not working, modified model

--- model/modelmlp.py
+++ model/modelmlp.py
@@ -10,11 +10,11 @@ class ModelMLP(keras.Model):
 
         self.increase_channels = layers.Conv2D(32, kernel_size=3, padding='same')
 
-        self.res_dec1 = ResidualBlock(activation, 32, is_encoder=True)
-        self.res_dec2 = ResidualBlock(activation, 64, is_encoder=True, resize_factor=1/2)
-        self.res_dec3 = ResidualBlock(activation, 64, is_encoder=True)
-        self.res_dec4 = ResidualBlock(activation, 128, is_encoder=True, resize_factor=1/2)
-        self.res_dec5 = ResidualBlock(activation, 128, is_encoder=True)
+        self.res_enc1 = ResidualBlock(activation, 32, is_encoder=True)
+        self.res_enc2 = ResidualBlock(activation, 64, is_encoder=True, resize_factor=0.5)
+        self.res_enc3 = ResidualBlock(activation, 64, is_encoder=True)
+        self.res_enc4 = ResidualBlock(activation, 128, is_encoder=True, resize_factor=0.5)
+        self.res_enc5 = ResidualBlock(activation, 128, is_encoder=True)
 
         self.res_dec1 = ResidualBlock(activation, 128, is_encoder=False)
         self.res_dec2 = ResidualBlock(activation, 128, is_encoder=False, resize_factor=2)
@@ -27,9 +27,10 @@ class ModelMLP(keras.Model):
     def build(self, input_shape):
         # Here we get the depth of the image that is passed to the model at the start, i.e. 1 for MNIST.
         self.in_shape = input_shape
-        self.decrease_channels = layers.Conv2D(input_shape[0][-1], kernel_size=3, strides=1, padding='same')
+        self.decrease_channels = layers.Conv2D(input_shape[-1], kernel_size=3, strides=1, padding='same')
 
-    def __call__(self, inputs, **kwargs):
+    def call(self, inputs, mask=None):
+        inputs = inputs
         out = self.increase_channels(inputs)
 
         out = self.res_enc1(out)
@@ -48,7 +49,8 @@ class ModelMLP(keras.Model):
 
         return out
 
-class ResidualBlock(layers.layer):
+
+class ResidualBlock(layers.Layer):
     def __init__(self, activation, filters, is_encoder, kernel_size=3, strides=1, resize_factor=None):
         super(ResidualBlock, self).__init__()
 
@@ -58,17 +60,19 @@ class ResidualBlock(layers.layer):
         self.activation = activation
 
         if is_encoder:
-            self.conv1 = layers.conv2D(filters, kernel_size, strides=strides, padding='same')
-            self.conv2 = layers.conv2D(filters, kernel_size, strides=strides, padding='same')
+            self.conv1 = layers.Conv2D(filters, kernel_size, strides=strides, padding='same')
+            self.conv2 = layers.Conv2D(filters, kernel_size, strides=strides, padding='same')
         else:
-            self.conv1 = layers.conv2DTranspose(filters, kernel_size, strides=strides, padding='same')
-            self.conv2 = layers.conv2DTranspose(filters, kernel_size, strides=strides, padding='same')
+            self.conv1 = layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding='same')
+            self.conv2 = layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding='same')
 
         self.resize_factor = resize_factor
+        self.increase_channels_skip = layers.Conv2D(filters, kernel_size=1, padding='valid')
+
 
     def call(self, inputs, **kwargs):
+        hh = inputs
         # TODO: CHECK IF INPUTS NEED TO BE SPLIT ?
-
         x = self.norm1(inputs)
         x = self.activation(x)
         x = self.conv1(x)
@@ -77,10 +81,14 @@ class ResidualBlock(layers.layer):
         x = self.conv2(x)
 
         # FIXME: THEY DO IT WITH STRIDE = 2. ALSO, THEY RESIZE x IN THE FIRS CONVOLUTION.
-        if self.resize_factor is not None:
-            x = tf.image.resize(x, x.shape[1:-1] * self.resize_factor)
-            skip_x = tf.image.resize(inputs, inputs.shape[1:-1] * self.resize_factor)  # TODO: CHECK IF OK?
+        if self.resize_factor:
+            new_dims = x.shape[1:-1] * tf.convert_to_tensor(self.resize_factor)
+            x = tf.image.resize(x, new_dims)
+            skip_x = tf.image.resize(inputs, new_dims)  # TODO: CHECK IF OK?
         else:
             skip_x = inputs
+
+        if x.shape != skip_x.shape:
+            skip_x = self.increase_channels_skip(skip_x)
 
         return skip_x + x
