6c069bf63ed2bd74f243a22365776067bdbce751
fnuzzo <fnuzzo@kth.se>
2019-10-23 13:41:36 +0200

    clean up generate.py

--- generate.py
+++ generate.py
@@ -69,7 +69,7 @@ def sample_many(model, sigmas, batch_size=128, eps=2 * 1e-5, T=100, n_images=1):
     :return: Tensor of dimensions (n_images, width, height, channels)
     """
     # Tuple for (n_images, width, height, channels)
-    image_size = (n_images) + model.in_shape[0][1:]
+    image_size = (n_images,) + utils.get_dataset_image_size(configs.config_values.dataset)
     batch_size = min(batch_size, n_images)
 
     with tf.device('CPU'):
@@ -78,7 +78,8 @@ def sample_many(model, sigmas, batch_size=128, eps=2 * 1e-5, T=100, n_images=1):
     x_processed = None
 
     n_processed_images = 0
-    for i_batch, batch in enumerate(tqdm(x, total=tf.data.experimental.cardinality(x).numpy(), desc='Generating samples')):
+    for i_batch, batch in enumerate(
+            tqdm(x, total=tf.data.experimental.cardinality(x).numpy(), desc='Generating samples')):
         for i, sigma_i in enumerate(sigmas):
             alpha_i = eps * (sigma_i / sigmas[-1]) ** 2
             idx_sigmas = tf.ones(batch.get_shape()[0], dtype=tf.int32) * i
@@ -96,7 +97,7 @@ def sample_many(model, sigmas, batch_size=128, eps=2 * 1e-5, T=100, n_images=1):
     return x_processed
 
 
-def sample_and_save(model, sigmas, image_size, eps=2 * 1e-5, T=100, n_images=1):
+def sample_and_save(model, sigmas, eps=2 * 1e-5, T=100, n_images=1):
     """
     Only for MNIST, for now.
     :param model:
@@ -105,7 +106,7 @@ def sample_and_save(model, sigmas, image_size, eps=2 * 1e-5, T=100, n_images=1):
     :param T:
     :return:
     """
-    image_size = (n_images,) + image_size
+    image_size = (n_images,) + utils.get_dataset_image_size(configs.config_values.dataset)
 
     x = tf.random.uniform(shape=image_size)
 
@@ -124,26 +125,18 @@ if __name__ == '__main__':
     args = utils.get_command_line_args()
     configs.config_values = args
 
+    save_dir = utils.get_savemodel_dir()
+    model, optimizer, step = utils.try_load_model(save_dir, verbose=False)
     start_time = datetime.now().strftime("%y%m%d-%H%M%S")
-    dataset_name = configs.config_values.dataset
-    model_directory = './saved_models/'
-    dataset = dataset_name + '/'
-    filters = 16
-    image_size = utils.get_dataset_image_size(configs.config_values.dataset)
 
-    step = tf.Variable(0)
-    model = RefineNet(filters=filters, activation=tf.nn.elu)
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    latest_checkpoint = tf.train.latest_checkpoint(model_directory + dataset)
-    print("loading model from checkpoint ", latest_checkpoint)
-    ckpt = tf.train.Checkpoint(step=step, optimizer=optimizer, model=model)
-    ckpt.restore(latest_checkpoint)
+    model_directory = './saved_models/'
 
     sigma_levels = tf.math.exp(tf.linspace(tf.math.log(1.0),
                                            tf.math.log(0.01),
                                            10))
 
-    samples_directory = './samples/' + f'{start_time}_{dataset_name}_{step.numpy()}steps_{filters}filters' + "/"  # TODO: add number of steps in name
+    samples_directory = './samples/' + f'{start_time}_{configs.config_values.dataset}' \
+        f'_{step}steps_{configs.config_values.filters}filters' + "/"
     os.makedirs(samples_directory)
 
-    samples = sample_and_save(model, sigma_levels, T=100, n_images=400, image_size=image_size)
+    samples = sample_and_save(model, sigma_levels, T=100, n_images=400)
