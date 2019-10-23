dc53688cbed483ba7fbe72c70ae1a32d767441d8
fnuzzo <fnuzzo@kth.se>
2019-10-23 13:25:06 +0200

    add baseline and better loading of the model

--- train.py
+++ train.py
@@ -1,19 +1,16 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import numpy as np
-import utils, os
+import os
 from tqdm import tqdm
 from datetime import datetime
-from model.inception import Metrics
-import cProfile
 
 # our files
+from model.inception import Metrics
 from datasets.dataset_loader import get_train_test_data
-from model.refinenet import RefineNet
-from losses.losses import loss_per_batch, loss_per_batch_alternative
+from losses.losses import loss_per_batch_alternative
 from generate import sample_many
 import configs
-from generate import plot_grayscale
+import utils
+
 
 def manage_gpu_memory_usage():
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -34,16 +31,6 @@ manage_gpu_memory_usage()
 device = utils.get_tensorflow_device()
 
 
-def print_model_summary(model):
-    batch = 2
-    if configs.config_values.dataset in ["cifar10", "celeb_a"]:
-        x = [tf.ones(shape=(batch, 32, 32, 3)), tf.ones(batch, dtype=tf.int32)]
-    else:
-        x = [tf.ones(shape=(batch, 28, 28, 1)), tf.ones(batch, dtype=tf.int32)]
-    out = model(x)
-    print(model.summary())
-
-
 @tf.function
 def train_one_step(model, optimizer, data_batch_perturbed, data_batch, idx_sigmas, sigmas):
     with tf.GradientTape() as t:
@@ -53,56 +40,42 @@ def train_one_step(model, optimizer, data_batch_perturbed, data_batch, idx_sigma
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return current_loss
 
-def train():   
+
+def train():
     # load dataset from tfds (or use downloaded version if exists)
     train_data, test_data = get_train_test_data(configs.config_values.dataset)
     num_examples = int(tf.data.experimental.cardinality(train_data))
 
     # split data into batches
-    train_data = train_data.shuffle(1000).batch(configs.config_values.batch_size).repeat().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-
-    num_batches = int(tf.data.experimental.cardinality(train_data))
-    num_filters = {'mnist': 16, 'cifar10': 64, 'celeb_a': 128} # NOTE change mnist back to 64
+    train_data = train_data.shuffle(1000).batch(configs.config_values.batch_size).repeat().prefetch(
+        buffer_size=tf.data.experimental.AUTOTUNE)
 
     # path for saving the model(s)
-    save_dir = configs.config_values.checkpoint_dir + configs.config_values.dataset + '/'
-    # if not os.path.exists(save_dir):
-        # os.makedirs(save_dir)
+    save_dir = utils.get_savemodel_dir()
 
     start_time = datetime.now().strftime("%y%m%d-%H%M")
-    log_dir = configs.config_values.log_dir + configs.config_values.dataset + "/"
-    summary_writer = tf.summary.create_file_writer(log_dir+start_time)
 
-    # initialize models
-    model = RefineNet(filters=num_filters[configs.config_values.dataset], activation=tf.nn.elu)
-    print_model_summary(model)
+    model, optimizer, step = utils.try_load_model(save_dir)
+
     metrics = Metrics()
-    # declare optimizer
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001) # NOTE 10 times larger than in their paper
-
-    # if resuming training, overwrite model parameters from checkpoint
-    if configs.config_values.resume:
-        latest_checkpoint = tf.train.latest_checkpoint(save_dir)
-        print("loading model from checkpoint ", latest_checkpoint)
-        step = tf.Variable(0)
-        ckpt = tf.train.Checkpoint(step=step, optimizer=optimizer, model=model)
-        ckpt.restore(latest_checkpoint)
-        step = int(step)
-    else:
-        step = 0
 
     # array of sigma levels
     # generate geometric sequence of values between sigma_low (0.01) and sigma_high (1.0)
-    sigma_levels = tf.math.exp(tf.linspace( tf.math.log(configs.config_values.sigma_high),
-                                            tf.math.log(configs.config_values.sigma_low),
-                                            configs.config_values.num_L ))
+    if configs.config_values.baseline:
+        sigma_levels = tf.ones(1) * configs.config_values.sigma_low
+        configs.config_values.num_L = 1
+    else:
+        sigma_levels = tf.math.exp(tf.linspace(tf.math.log(configs.config_values.sigma_high),
+                                               tf.math.log(configs.config_values.sigma_low),
+                                               configs.config_values.num_L))
 
     # Compute inception score mean and standard deviation
-    sampled_images = sample_many(model, sigma_levels, n_images=10)
+    sampled_images = sample_many(model, sigma_levels, n_images=2176)
     is_mean, is_stddev = metrics.compute_inception_score(sampled_images, image_side_inception=199)
     print(f'Inception Score: {is_mean:.3} +- {is_stddev:.3}')
     # Compute fid
-    fid = metrics.compute_fid(images_1=sampled_images, data_2=test_data, image_side_inception=199)
+    test_data_inception = test_data.take(2176)
+    fid = metrics.compute_fid(images_1=sampled_images, data_2=test_data_inception, image_side_inception=199)
     print(f'FID Score: {fid:.3}')
 
     # training loop
@@ -112,16 +85,16 @@ def train():
           f'training...')
 
     total_steps = configs.config_values.steps
-    progress_bar = tqdm(train_data, total=total_steps, initial=step+1)
+    progress_bar = tqdm(train_data, total=total_steps, initial=step + 1)
     progress_bar.set_description(f'iteration {step}/{total_steps} | current loss ?')
 
-    with tf.device(device): # For some reason, this makes everything faster
+    with tf.device(device):  # For some reason, this makes everything faster
         avg_loss = 0
         for data_batch in progress_bar:
             step += 1
             idx_sigmas = tf.random.uniform([data_batch.shape[0]], minval=0,
-                                                maxval=configs.config_values.num_L,
-                                                dtype=tf.dtypes.int32)
+                                           maxval=configs.config_values.num_L,
+                                           dtype=tf.dtypes.int32)
             sigmas = tf.gather(sigma_levels, idx_sigmas)
             sigmas = tf.reshape(sigmas, shape=(data_batch.shape[0], 1, 1, 1))
             data_batch_perturbed = data_batch + tf.random.normal(shape=data_batch.shape) * sigmas
@@ -137,17 +110,14 @@ def train():
                 # TODO: maybe save also info about the sigmas
                 ckpt = tf.train.Checkpoint(step=tf.Variable(0), optimizer=optimizer, model=model)
                 ckpt.step.assign_add(step)
-                ckpt.save(save_dir+f"{start_time}_step_{step}")
-                print(f"\nSaved checkpoint. Average loss: {avg_loss/configs.config_values.checkpoint_freq:.3f}")
+                ckpt.save(save_dir + f"{start_time}_step_{step}")
+                print(f"\nSaved checkpoint. Average loss: {avg_loss / configs.config_values.checkpoint_freq:.3f}")
                 avg_loss = 0
             if step == total_steps:
                 return
 
 
-
-
 if __name__ == "__main__":
-
     tf.get_logger().setLevel('ERROR')
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
